9627a10861d60d207eee99993f4e99dbfd77c422
iMichealgit1 <molatunbosun7@gmail.com>
2023-07-13 16:18:15 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Description - A C program that prints a line to the standard error
- * Return 1(success)
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	/*
-	 * your codes goes her
-	 */
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
